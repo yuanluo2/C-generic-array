19fb694f9778cc98e6bff162df40a50ce255b4c4
Ge Zhenyuan <49439486+yuanluo2@users.noreply.github.com>
2023-02-03 13:50:04 +0800

    Update array.h

--- array.h
+++ array.h
@@ -10,51 +10,51 @@ typedef enum Status{
 	STATUS_UNKNOWN
 } Status;
 
-#define ARRAY_TEMPLATE(T)       \
+#define ARRAY_TEMPLATE(T)           \
 	typedef struct Array_##T{   \
-		T* data;                \
-		size_t capacity;        \
-		size_t length;          \
+		T* data;            \
+		size_t capacity;    \
+		size_t length;      \
 	} Array_##T;                \
-							                                    \
-	void Array_##T##_init(Array_##T* arr, Status* status){      \
+							                \
+	void Array_##T##_init(Array_##T* arr, Status* status){          \
 		(arr)->capacity = 5;                                    \
 		(arr)->length = 0;                                      \
 		(arr)->data = (T*)malloc(sizeof(T) * (arr)->capacity);  \
-											                    \
+									\
 		if (NULL == (arr)->data){                               \
-			*status = STATUS_OUT_OF_MEMORY;                     \
+			*status = STATUS_OUT_OF_MEMORY;                 \
 		}                                                       \
-		else{									                \
-			*status = STATUS_OK;                                \
+		else{							\
+			*status = STATUS_OK;                            \
 		}                                                       \
-	}                                                           \
+	}                                                               \
 	                                                    \
 	T* Array_##T##_at(Array_##T* arr, size_t index){    \
-		return &((arr)->data[index]);                   \
+		return &((arr)->data[index]);               \
 	}                                                   \
 	                                                                        \
 	T* Array_##T##_check_at(Array_##T* arr, size_t index, Status* status){  \
-		if (index >= (arr)->length){                                        \
-			*status = STATUS_SUBSCRIPT_OUT_OF_INDEX;                        \
-			return NULL;                                                    \
-		}                                                                   \
-																			\
-		return Array_##T##_at((arr), index);                                \
+		if (index >= (arr)->length){                                    \
+			*status = STATUS_SUBSCRIPT_OUT_OF_INDEX;                \
+			return NULL;                                            \
+		}                                                               \
+										\
+		return Array_##T##_at((arr), index);                            \
 	}                                                                       \
-	                                                                            \
-	void Array_##T##_add_back(Array_##T* arr, T elem, Status* status){          \
-		if ((arr)->length == (arr)->capacity){                                  \
+	                                                                                    \
+	void Array_##T##_add_back(Array_##T* arr, T elem, Status* status){                  \
+		if ((arr)->length == (arr)->capacity){                                      \
 			T* temp = (T*)realloc(arr->data, sizeof(T) * 2 * (arr)->capacity);  \
 			if (NULL == temp){                                                  \
-				*status = STATUS_OUT_OF_MEMORY;                                 \
-				return;                                                         \
+				*status = STATUS_OUT_OF_MEMORY;                             \
+				return;                                                     \
 			}                                                                   \
-																				\
+											    \
 			(arr)->data = temp;                                                 \
 			(arr)->capacity *= 2;                                               \
 		}                                                                       \
-																				\
+											\
 		((arr)->data)[((arr)->length)] = elem;                                  \
 		++((arr)->length);                                                      \
 		*status = STATUS_OK;                                                    \
